75ddc5602206b349f9619a0b506d5542c5ee8c45
GowthamaViknesh <gowthampostbox18@gmail.com>
2023-07-12 21:11:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Persondeatils from "./components/Persondeatils";
+
+function App() {
+  return (
+    <div className="main">
+      <h1>React Crud Operations</h1>
+      <h4>Create Update Delete Edit</h4>
+      <Persondeatils />
+    </div>
+  );
+}
+
+export default App;
